794aac343d58f1df8b6005b2057de8327c903b63
Scott Woods <scott@westarete.com>
2010-01-06 22:17:06 -0500

    Create delayed_jobs table.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090911211238) do
+ActiveRecord::Schema.define(:version => 20100107030157) do
 
   create_table "adjustments", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(:version => 20090911211238) do
 
   create_table "buckets", :force => true do |t|
     t.string   "permalink"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
